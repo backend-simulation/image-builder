58fbd2612e78d800674e8c7ed5e123d39008fbee
bron.kang <bron.kang@kakaoenterprise.com>
2023-01-01 16:55:20 +0900

    hotfix: Improve test message

--- internal/pkg/gitclone/git_clone_test.go
+++ internal/pkg/gitclone/git_clone_test.go
@@ -7,18 +7,18 @@ import (
 
 func TestGitClone(t *testing.T) {
 	defer os.RemoveAll(tempRootPath)
-	info := GitClone(tempRootPath+"/image-builder", "https://github.com/backend-simulation/image-builder.git")
+	info := GitClone(tempRootPath+"/image-builder", "https://github.com/backend-simulation/sample-codes.git")
 
 	_, err := os.Stat(info.Path)
 	if err != nil {
 		if os.IsNotExist(err) {
 			t.Fatalf("not exists: %s", err.Error())
 		} else {
-			t.Logf("file exists: %s", err.Error())
+			t.Logf("file already exists: %s", err.Error())
 		}
 	}
 
-	t.Log("file exists")
+	t.Log("file git clone successful")
 	t.Logf("hash is %s", info.Tag)
 }
 
